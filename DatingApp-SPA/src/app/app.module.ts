f53a9dd5f11e9bd5ba98de40b4bbf1cf03c88581
Atataika <bychkov_egor@mail.ru>
2020-08-29 16:24:10 +0300

    Ngx bootstrap import

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/home/components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,7 +13,14 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, SharedModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent],
 })
